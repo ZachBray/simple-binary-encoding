b9438ee407eb4043713527f7889be2b2da55a191
Zach Bray <zachbray@googlemail.com>
2018-07-18 18:53:31 +0100

    Add test around JS integer serialization.

--- sbe-tool/src/test/java/uk/co/real_logic/sbe/generation/typescript/TypeScriptGeneratorTest.java
+++ sbe-tool/src/test/java/uk/co/real_logic/sbe/generation/typescript/TypeScriptGeneratorTest.java
@@ -1,6 +1,10 @@
 package uk.co.real_logic.sbe.generation.typescript;
 
+import static org.junit.Assert.assertEquals;
 import static uk.co.real_logic.sbe.xml.XmlSchemaParser.parse;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 import org.agrona.generation.StringWriterOutputManager;
 import org.junit.Test;
@@ -33,6 +37,30 @@ public class TypeScriptGeneratorTest
     public void shouldNotThrowWhenGeneratingCodeFromExtendedSchema() throws Exception
     {
         generateFromSchema("extension-schema.xml");
+    }
+
+    @Test
+    public void shouldGenerateJavaCompatibleNegative53BitLongs()
+    {
+        final long expectedValue = -9007199254740991L; // Min safe JS integer
+        final ByteBuffer buffer = ByteBuffer.wrap(new byte[] {
+            (byte)255, (byte)224, 0, 0, 0, 0, 0, 1
+        });
+        buffer.order(ByteOrder.BIG_ENDIAN);
+        final long observedValue = buffer.getLong(0);
+        assertEquals(expectedValue, observedValue);
+    }
+
+    @Test
+    public void shouldGenerateJavaCompatiblePositive53BitLongs()
+    {
+        final long expectedValue = 9007199254740991L; // Min safe JS integer
+        final ByteBuffer buffer = ByteBuffer.wrap(new byte[] {
+            0, 31, (byte)255, (byte)255, (byte)255, (byte)255, (byte)255, (byte)255
+        });
+        buffer.order(ByteOrder.BIG_ENDIAN);
+        final long observedValue = buffer.getLong(0);
+        assertEquals(expectedValue, observedValue);
     }
 
     private void generateFromSchema(final String schemaResource) throws Exception
